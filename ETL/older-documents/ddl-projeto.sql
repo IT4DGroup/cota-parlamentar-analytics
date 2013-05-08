fd10d34b9e97ad306afdf2743b6a5c1fc1692cde
Anderson Katao <akatao@ubuntu.(none)>
2013-05-08 12:02:50 -0300

    Organização da pasta ETL: 1. Pasta Pedro renomeada para older-documents; 2. Arquivos da pasta Renato (Ultima revisão dos ETLs) estão na raiz do diretorio ETL; 3. Diretorio source-files movido para a pasta ETL; 4. Diretorio etls removido.

---
+++ ETL/older-documents/ddl-projeto.sql
@@ -0,0 +1,31 @@
+CREATE TABLE stage.stg_xmlcotaparlamentar
+(
+  txNomeParlamentar TEXT
+, nuCarteiraParlamentar DOUBLE PRECISION
+, nuLegislatura DOUBLE PRECISION
+, sgUF TEXT
+, sgPartido TEXT
+, codLegislatura DOUBLE PRECISION
+, numSubCota DOUBLE PRECISION
+, txtDescricao TEXT
+, txtBeneficiario TEXT
+, txtCNPJCPF TEXT
+, txtNumero TEXT
+, indTipoDocumento DOUBLE PRECISION
+, datEmissao TEXT
+, vlrDocumento DOUBLE PRECISION
+, vlrGlosa DOUBLE PRECISION
+, vlrLiquido DOUBLE PRECISION
+, numMes DOUBLE PRECISION
+, numAno DOUBLE PRECISION
+, numParcela DOUBLE PRECISION
+, numLote DOUBLE PRECISION
+, numRessarcimento DOUBLE PRECISION
+, numEspecificacaoSubCota DOUBLE PRECISION
+, txtDescricaoEspecificacao TEXT
+, txtPassageiro TEXT
+, txtTrecho TEXT
+)
+;
+
+
